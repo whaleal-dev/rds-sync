b95e0ca1330fad9acb3620b581766a8941d23ccc
GitHubLhp123 <2446268838@qq.com>
2021-08-26 16:41:21 +0800

    更新数据源的创建方式

--- common/src/main/java/dbconnection/mongodb/MongoDbConnection.java
+++ common/src/main/java/dbconnection/mongodb/MongoDbConnection.java
@@ -2,6 +2,8 @@ package dbconnection.mongodb;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import common.photonV.entity.Datasource;
+import dbconnection.mysql.MySqlConnection;
 import org.bson.Document;
 
 import java.util.Map;
@@ -29,13 +31,30 @@ public class MongoDbConnection {
         if (mongoClientMap.containsKey(dsName)) {
             return;
         }
-        System.out.println("数据源启动:"+dsName);
+        System.out.println("数据源启动:" + dsName);
         MongoClient mongoClient = MongoClients.create(dsName);
         mongoClientMap.put(dsName, mongoClient);
 
         System.out.println(dsName + "数据源启动成功");
     }
 
+    public static synchronized void createMonoDbClient(String dsName, Datasource datasource) {
+        if (mongoClientMap.containsKey(dsName)) {
+            return;
+        }
+        System.out.println("数据源启动:" + dsName);
+        MongoClient mongoClient = MongoClients.create(datasource.getUrl());
+        mongoClientMap.put(dsName, mongoClient);
+
+        System.out.println(dsName + "数据源启动成功");
+    }
+
+    public static MongoClient getMongoClient(String dsName, Datasource datasource) {
+        if (!mongoClientMap.containsKey(dsName)) {
+            createMonoDbClient(dsName, datasource);
+        }
+        return mongoClientMap.get(dsName);
+    }
 
     /**
      * getMongoClient 获取mongodb客户端
@@ -51,6 +70,7 @@ public class MongoDbConnection {
         return mongoClientMap.get(dsName);
     }
 
+
     /**
      * close 关闭mongodb客户端
      *
@@ -64,7 +84,26 @@ public class MongoDbConnection {
     }
 
     public static void main(String[] args) {
-       getMongoClient("mongodb://admin:123456@192.168.3.172:6001/admin?authSource=admin");
+        //getMongoClient("mongodb://admin:123456@192.168.3.172:6001/admin?authSource=admin");
+        Map<String, Object> map = MySqlConnection.getJdbcTemplate("1").queryForMap("select * from photon.datasource where name='mongodb1' ");
+
+        Datasource datasource = new Datasource();
+        datasource.setId(map.get("id").toString());
+        datasource.setName(map.get("name").toString());
+        datasource.setType(map.get("type").toString());
+        datasource.setDsDatabase(map.get("ds_database").toString());
+        datasource.setUsername(map.get("username").toString());
+        datasource.setPassword(map.get("password").toString());
+
+        datasource.setId(map.get("ip").toString());
+
+
+        datasource.setUrl(map.get("url").toString());
+
+        datasource.setPort((String) map.get("port"));
+
+
+        getMongoClient("1",datasource);
     }
 
 }
--- common/src/main/java/dbconnection/mysql/MySqlConnection.java
+++ common/src/main/java/dbconnection/mysql/MySqlConnection.java
@@ -2,6 +2,7 @@ package dbconnection.mysql;
 
 
 import com.mongodb.client.MongoClient;
+import common.photonV.entity.Datasource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -41,6 +42,24 @@ public class MySqlConnection {
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+        System.out.println(dsName + "数据源启动成功");
+    }
+
+    private static synchronized void getBasicDataSource(String dsName, Datasource datasource) {
+        if (jdbcTemplateMysqlMap.containsKey(dsName)) {
+            return;
+        }
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(datasource.getUrl());
+        basicDataSource.setUsername(datasource.getUsername());
+        basicDataSource.setPassword(datasource.getPassword());
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(basicDataSource);
+        jdbcTemplateMysqlMap.put(dsName, jdbcTemplate);
+        try {
+            connectionMysqlMap.put(dsName, basicDataSource.getConnection());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     /**
@@ -53,6 +72,29 @@ public class MySqlConnection {
     public static Connection getConnection(String dsName) {
         if (!jdbcTemplateMysqlMap.containsKey(dsName)) {
             getBasicDataSource(dsName);
+        }
+        synchronized (MySqlConnection.class) {
+            if (!connectionMysqlMap.containsKey(dsName)) {
+                try {
+                    connectionMysqlMap.put(dsName, jdbcTemplateMysqlMap.get(dsName).getDataSource().getConnection());
+                } catch (SQLException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        return connectionMysqlMap.get(dsName);
+    }
+
+    /**
+     * getJdbcTemplate 获取mysql的connection
+     *
+     * @param dsName
+     * @return JdbcTemplate
+     * @desc 获取mysql的Jdbc
+     */
+    public static Connection getConnection(String dsName, Datasource datasource) {
+        if (!jdbcTemplateMysqlMap.containsKey(dsName)) {
+            getBasicDataSource(dsName, datasource);
         }
         synchronized (MySqlConnection.class) {
             if (!connectionMysqlMap.containsKey(dsName)) {
@@ -80,6 +122,12 @@ public class MySqlConnection {
         return jdbcTemplateMysqlMap.get(dsName);
     }
 
+    public static JdbcTemplate getJdbcTemplate(String dsName, Datasource datasource) {
+        if (!jdbcTemplateMysqlMap.containsKey(dsName)) {
+            getBasicDataSource(dsName, datasource);
+        }
+        return jdbcTemplateMysqlMap.get(dsName);
+    }
 
     /**
      * close 关闭jdbc链接
@@ -95,5 +143,28 @@ public class MySqlConnection {
                 exception.printStackTrace();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        //getMongoClient("mongodb://admin:123456@192.168.3.172:6001/admin?authSource=admin");
+        Map<String, Object> map = MySqlConnection.getJdbcTemplate("1").queryForMap("select * from photon.datasource where name='mysql1' ");
+
+        Datasource datasource = new Datasource();
+        datasource.setId(map.get("id").toString());
+        datasource.setName(map.get("name").toString());
+        datasource.setType(map.get("type").toString());
+        datasource.setDsDatabase(map.get("ds_database").toString());
+        datasource.setUsername(map.get("username").toString());
+        datasource.setPassword(map.get("password").toString());
+
+        datasource.setId(map.get("ip").toString());
+
+
+        datasource.setUrl(map.get("url").toString());
+
+        datasource.setPort((String) map.get("port"));
+
+
+        getJdbcTemplate("1",datasource);
     }
 }
