25fa822a3d1d14812b59d96a4e4702d8672eb10e
GitHubLhp123 <2446268838@qq.com>
2021-08-31 10:12:48 +0800

    修复一个bug

--- core/src/main/java/trigger/TriggerUtil.java
+++ core/src/main/java/trigger/TriggerUtil.java
@@ -1,6 +1,7 @@
 package trigger;
 
 import common.photonV.entity.TaskTrigger;
+import dbconnection.MetadataConnection;
 import dbconnection.mysql.MySqlConnection;
 import util.StringUtil;
 
@@ -16,16 +17,16 @@ public class TriggerUtil {
     public static void insertTriggerInfo(TaskTrigger taskTrigger) {
         String sql = "insert into photon.task_trigger(id,task_name,proc_name,state)";
         String values = "values(?,?,?,?)";
-        MySqlConnection.getJdbcTemplate("1").update(sql + values, taskTrigger.getId(), taskTrigger.getTaskName(), taskTrigger.getProcName(), "new");
+        MetadataConnection.getJdbcTemplate().update(sql + values, taskTrigger.getId(), taskTrigger.getTaskName(), taskTrigger.getProcName(), "new");
     }
 
     public static void updateTriggerInfo(TaskTrigger taskTrigger) {
         String sql = "update  photon.task_trigger set state=? where id=?";
-        MySqlConnection.getJdbcTemplate("1").update(sql, taskTrigger.getState(), taskTrigger.getId());
+        MetadataConnection.getJdbcTemplate().update(sql, taskTrigger.getState(), taskTrigger.getId());
     }
 
     public static String getTriggerTateInfo(String id) {
         String sql = "select state  photon.task_trigger where id='" + id + "'";
-        return MySqlConnection.getJdbcTemplate("1").queryForObject(sql, String.class);
+        return MetadataConnection.getJdbcTemplate().queryForObject(sql, String.class);
     }
 }
