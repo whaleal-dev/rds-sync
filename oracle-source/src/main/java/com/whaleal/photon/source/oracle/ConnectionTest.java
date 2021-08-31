78ea0cf4334a17c03ad885a9463ce73ca2d10132
拽拽 <1050564479@qq.com>
2021-08-31 15:48:34 +0800

    oracle-read补充

--- oracle-source/src/main/java/com/whaleal/photon/source/oracle/ConnectionTest.java
+++ oracle-source/src/main/java/com/whaleal/photon/source/oracle/ConnectionTest.java
@@ -6,22 +6,24 @@ import dbconnection.oracle.OracleConnection;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Connection;
+import java.util.List;
 import java.util.Map;
-
-import static dbconnection.oracle.OracleConnection.getConnection;
 
 public class ConnectionTest {
     public static void main(String[] args) {
-        //getMongoClient("mongodb://admin:123456@192.168.3.172:6001/admin?authSource=admin");
-        //Map<String, Object> map = OracleConnection.getJdbcTemplate("1").queryForMap("select * from photon.datasource where name='mysql1' ");
+
         Datasource proc4 = DBUtil.getSourceByProcName("proc4");
         Connection connection = OracleConnection.createConnection(proc4);
         JdbcTemplate jdbcTemplate = OracleConnection.getJdbcTemplateBySource(proc4);
-        Map<String, Object> map = jdbcTemplate.queryForMap("select * from CS.test");
+        List<Map<String, Object>> dbTableList = jdbcTemplate.queryForList("select t.table_name,t.num_rows from USER_TABLES t;");
+        for (Map dbTableMap : dbTableList) {
+            String dbName = dbTableMap.get("TABLE_SCHEMA").toString();
+            String tableName = dbTableMap.get("TABLE_NAME").toString();
+            System.out.println(tableName);
+        }
+        Map<String, Object> map = jdbcTemplate.queryForMap("select * from STU");
         map.entrySet().forEach(item -> {
             System.out.println(item.getKey() + "-----" + item.getValue());
         });
-        System.out.println(connection);
-        System.out.println(proc4.getDsDatabase());
     }
 }
